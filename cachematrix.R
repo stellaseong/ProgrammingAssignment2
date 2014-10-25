00ef9128a661e80e2b17eb0ca6ea9c890c9738b3
Stella <stella.seong@users.noreply.github.com>
2014-10-25 17:43:26 -0500

    R file

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,8 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+
 }
 
 
@@ -12,4 +14,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+
+weightmedian <- function(directory, day, id=1:3) {
+files_list <- list.files(directory, full.names=TRUE) #creates a list of files
+dat <- data.frame() #creates an empty data frame
+for (i in id) { #loops through the files, rbinding them together
+dat <- rbind(dat, read.csv(files_list[i]))
+}
+dat_subset <- dat[which(dat[, "Day"] == day),] #subsets the rows that match the 'day' argument
+median(dat_subset[, "Weight"], na.rm=TRUE) #identifies the median weight
+#while stripping out the NAs
 }
